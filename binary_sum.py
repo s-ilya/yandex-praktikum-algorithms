959088deacb5985be74c464137f68c7e3c52c74e
Ilia Sarantsev <ilya.sarantsev@gmail.com>
2020-06-19 09:52:54 +0300

    Binary sum optimized (not really)

--- binary_sum.py
+++ binary_sum.py
@@ -1,54 +1,45 @@
-from datetime import datetime
+from collections import deque
 
 
 def binary_sum(left: str, right: str) -> str:
-    # checkpoint = datetime.now().microsecond
+    left_numbers = list(map(int, left))
+    right_numbers = list(map(int, right))
 
-    left_index = len(left) - 1
-    right_index = len(right) - 1
+    left_index = len(left_numbers) - 1
+    right_index = len(right_numbers) - 1
     carried_digit = 0
-    result = ""
-
-    # print("After initialization: " + str((datetime.now().microsecond - checkpoint) / 1000))
-    # checkpoint = datetime.now().microsecond
+    result = deque()
 
     while left_index >= 0 and right_index >= 0:
-        local_sum = int(left[left_index]) + int(right[right_index]) + carried_digit
+        sum_carried_tuple = sum_digits(left_numbers[left_index],
+                                       right_numbers[right_index],
+                                       carried_digit)
 
-        result = ("1" if local_sum == 1 or local_sum == 3 else "0") + result
-        carried_digit = 1 if local_sum > 1 else 0
+        result.appendleft(sum_carried_tuple[0])
+        carried_digit = sum_carried_tuple[1]
         left_index -= 1
         right_index -= 1
-
-    # print("After left_index >= 0 and right_index >= 0: " + str((datetime.now().microsecond - checkpoint) / 1000))
-    # checkpoint = datetime.now().microsecond
 
     while left_index >= 0:
-        local_sum = int(left[left_index]) + carried_digit
+        sum_carried_tuple = sum_digits(left_numbers[left_index], 0,
+                                       carried_digit)
 
-        result = ("1" if local_sum == 1 or local_sum == 3 else "0") + result
-        carried_digit = 1 if local_sum > 1 else 0
+        result.appendleft(sum_carried_tuple[0])
+        carried_digit = sum_carried_tuple[1]
         left_index -= 1
 
-    # print("After left_index >= 0: " + str((datetime.now().microsecond - checkpoint) / 1000))
-    # checkpoint = datetime.now().microsecond
-
     while right_index >= 0:
-        local_sum = int(right[right_index]) + carried_digit
+        sum_carried_tuple = sum_digits(0, right_numbers[right_index],
+                                       carried_digit)
 
-        result = ("1" if local_sum == 1 or local_sum == 3 else "0") + result
-        carried_digit = 1 if local_sum > 1 else 0
+        result.appendleft(sum_carried_tuple[0])
+        carried_digit = sum_carried_tuple[1]
         right_index -= 1
 
-    # print("After right_index >= 0: " + str((datetime.now().microsecond - checkpoint) / 1000))
-    # checkpoint = datetime.now().microsecond
-
     if carried_digit != 0:
-        result = str(carried_digit) + result
+        result.appendleft(carried_digit)
 
-    # print("After carried_digit != 0: " + str((datetime.now().microsecond - checkpoint) / 1000))
-
-    return result
+    return "".join(map(str, result))
 
 
 def sum_digits(a, b, carried):
@@ -57,12 +48,4 @@ def sum_digits(a, b, carried):
 
 
 if __name__ == '__main__':
-    # first = format(2147483647, 'b')
-
-    # start = datetime.now().microsecond
-    # binary_sum_result = binary_sum(first, first)
-    # end = datetime.now().microsecond
-    #
-    # print(binary_sum_result)
-    # print((end - start) / 1000)
     print(binary_sum(input(), input()))
