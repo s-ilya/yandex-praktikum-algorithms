a6cfbcfafdae54ef9c19736dd06854ff83f94839
Ilia Sarantsev <ilya.sarantsev@gmail.com>
2020-06-19 10:46:55 +0300

    Three days stats solution

---
+++ tests/test_three_days_stats.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from three_days_stats import three_days_stats
+
+
+class ThreeDaysStatsTest(TestCase):
+    def test_three_days_stats(self):
+        self.assertEqual(
+            -1,
+            three_days_stats([1])
+        )
+        self.assertEqual(
+            -1,
+            three_days_stats([1, 2])
+        )
+        self.assertEqual(
+            -1,
+            three_days_stats([1, 2, 0])
+        )
+        self.assertEqual(
+            -1,
+            three_days_stats([1, 2, -3])
+        )
+        self.assertEqual(
+            12,
+            three_days_stats([-1, 4, -3])
+        )
+        self.assertEqual(
+            6,
+            three_days_stats([-2, -1, 3, -1, -2])
+        )
+        self.assertEqual(
+            -1,
+            three_days_stats([-2, 1, 1, -1, -2])
+        )
---
+++ three_days_stats.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def three_days_stats(stats: List[int]) -> int:
+    max_multiplication = -1
+
+    for first_index in range(0, len(stats)):
+        for second_index in range(first_index, len(stats)):
+            for third_index in range(second_index, len(stats)):
+                first_value = stats[first_index]
+                second_value = stats[second_index]
+                third_value = stats[third_index]
+
+                if (first_value + second_value + third_value) == 0:
+                    new_max = first_value * second_value * third_value
+                    max_multiplication = new_max if new_max > max_multiplication and new_max > 0 else max_multiplication
+
+    return max_multiplication
+
+
+if __name__ == '__main__':
+    _ = input()
+    print(str(three_days_stats(list(map(int, input().split(" "))))))
