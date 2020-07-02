21ddd33b159c4ea930a7bcf57abb501f75bfdd8b
Ilia Sarantsev <ilya.sarantsev@gmail.com>
2020-07-02 19:49:05 +0300

    Stonks solution

--- sprint_2/b_stonks.py
+++ sprint_2/b_stonks.py
@@ -1,12 +1,29 @@
-from typing import List
+def max_income(stonks):
+    bying_mode = 'bying'
+    selling_mode = 'selling'
 
-
-def max_income(stonks: List[int]) -> int:
     account = 0
+    mode = bying_mode
 
-    while len(stonks) >= 2:
-        account -= stonks.pop(0)
-        account += stonks.pop(0)
+    for today_index in range(len(stonks)):
+        today_value = stonks[today_index]
+
+        tomorrow_index = today_index + 1
+        tomorrow_value = stonks[tomorrow_index] if tomorrow_index < len(stonks) else -1
+
+        if mode == bying_mode and tomorrow_value > today_value:
+            account -= today_value
+            mode = selling_mode
+        elif mode == selling_mode and today_value > tomorrow_value:
+            account += today_value
+            mode = bying_mode
 
     return account
 
+
+if __name__ == '__main__':
+    _ = int(input())
+
+    print(max_income([
+        int(price) for price in input().split(' ')
+    ]))
--- tests/sprint_2/test_b_stonks.py
+++ tests/sprint_2/test_b_stonks.py
@@ -12,3 +12,15 @@ class MaxIncomeTest(TestCase):
 
     def test_two(self):
         self.assertEqual(1, max_income([0, 1]))
+
+    def test_skip_middle(self):
+        self.assertEqual(2, max_income([1, 2, 3]))
+
+    def test_skip_first(self):
+        self.assertEqual(1, max_income([3, 1, 2]))
+
+    def test_skip_last(self):
+        self.assertEqual(2, max_income([1, 2, 3, 2]))
+
+    def test_skip_all(self):
+        self.assertEqual(0, max_income([3, 2, 1]))
