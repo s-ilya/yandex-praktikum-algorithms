f54c212c4839f7d87f25bc3fac9d125588cab366
Ilia Sarantsev <ilya.sarantsev@gmail.com>
2020-07-19 17:56:55 +0300

    Median solution

---
+++ sprint_3/b_median.py
@@ -0,0 +1,62 @@
+from random import randrange
+from bisect import bisect_left, bisect_right
+
+
+def __order_stats(first: list, second: list, k: int) -> int:
+    overall_len = len(first) + len(second)
+    pivot_index = randrange(overall_len)
+
+    pivot = first[pivot_index] if pivot_index < len(first) else second[pivot_index - len(first)]
+
+    left_first = first[:bisect_left(first, pivot)]
+    left_second = second[:bisect_left(second, pivot)]
+
+    right_first = first[bisect_right(first, pivot):]
+    right_second = second[bisect_right(second, pivot):]
+
+    left_len = len(left_first) + len(left_second)
+    right_len = len(right_first) + len(right_second)
+    pivot_len = overall_len - left_len - right_len
+
+    if k <= left_len:
+        return __order_stats(left_first, left_second, k)
+    elif left_len < k <= left_len + pivot_len:
+        return pivot
+    else:
+        return __order_stats(right_first, right_second, k - left_len - pivot_len)
+
+
+def median(first: list, second: list) -> float:
+    if first is None:
+        first = []
+
+    if second is None:
+        second = []
+
+    overall_len = len(first) + len(second)
+
+    assert overall_len != 0
+
+    # k-th order statistics index starts from 1
+    if overall_len % 2 == 1:
+        return __order_stats(first, second, (overall_len // 2) + 1)
+    else:
+        median_first_k = (overall_len // 2)
+        median_second_k = (overall_len // 2) + 1
+        return (
+                       __order_stats(first, second, median_first_k) +
+                       __order_stats(first, second, median_second_k)
+               ) / 2
+
+
+if __name__ == '__main__':
+    with open('input.txt', mode='r') as input_txt, open('output.txt', mode='w') as output_txt:
+        input_txt.readline()
+        input_txt.readline()
+
+        output_txt.write(
+            '{:g}'.format(median(
+                [int(n) for n in input_txt.readline().split(' ')],
+                [int(n) for n in input_txt.readline().split(' ')]
+            ))
+        )
---
+++ tests/sprint_3/test_b_median.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from sprint_3.b_median import median
+
+
+class MedianTest(TestCase):
+    def test_empty(self):
+        with self.assertRaises(AssertionError):
+            median([], [])
+
+    def test_single_left(self):
+        self.assertEqual(1, median([1], []))
+
+    def test_single_right(self):
+        self.assertEqual(1, median([], [1]))
+
+    def test_odd_not_overlapped(self):
+        self.assertEqual(23, median([7, 23], [49]))
+
+    def test_odd_overlapped(self):
+        self.assertEqual(23, median([23], [7, 49]))
+
+    def test_even_not_overlapped(self):
+        self.assertEqual(36, median([23, 35], [37, 79]))
+
+    def test_even_overlapped(self):
+        self.assertEqual(36, median([23, 79], [35, 37]))
+
+    def test_sample(self):
+        self.assertEqual(2, median([1, 3], [2]))
+        self.assertEqual(2.5, median([1, 2], [3, 4]))
+        self.assertEqual(5, median(
+            [0, 0, 0, 1, 3, 3, 5, 10],
+            [4, 4, 5, 7, 7, 7, 8, 9, 9, 10]
+        ))
