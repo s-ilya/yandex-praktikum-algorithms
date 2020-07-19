7dbccf15009cd4bcb25f469b9fa0e7fe9fd4c575
Ilia Sarantsev <ilya.sarantsev@gmail.com>
2020-07-19 18:51:32 +0300

    Median speed improvement

--- sprint_3/b_median.py
+++ sprint_3/b_median.py
@@ -2,31 +2,59 @@ from random import randrange
 from bisect import bisect_left, bisect_right
 
 
-def __order_stats(first: list, second: list, k: int) -> int:
-    overall_len = len(first) + len(second)
+def __order_stats(
+        first: list,
+        first_start: int,
+        first_end: int,
+        second: list,
+        second_start: int,
+        second_end: int,
+        k: int
+):
+    first_len = first_end - first_start
+    second_len = second_end - second_start
+    overall_len = first_len + second_len
+
     pivot_index = randrange(overall_len)
 
-    pivot = first[pivot_index] if pivot_index < len(first) else second[pivot_index - len(first)]
+    pivot = first[first_start + pivot_index] if pivot_index < first_len else second[
+        second_start + (pivot_index - first_len)]
 
-    left_first = first[:bisect_left(first, pivot)]
-    left_second = second[:bisect_left(second, pivot)]
+    left_first_end = bisect_left(first, pivot, lo=first_start, hi=first_end)
+    left_second_end = bisect_left(second, pivot, lo=second_start, hi=second_end)
 
-    right_first = first[bisect_right(first, pivot):]
-    right_second = second[bisect_right(second, pivot):]
+    right_first_start = bisect_right(first, pivot, lo=first_start, hi=first_end)
+    right_second_start = bisect_right(second, pivot, lo=second_start, hi=second_end)
 
-    left_len = len(left_first) + len(left_second)
-    right_len = len(right_first) + len(right_second)
+    left_len = (left_first_end - first_start) + (left_second_end - second_start)
+    right_len = (first_end - right_first_start) + (second_end - right_second_start)
     pivot_len = overall_len - left_len - right_len
 
     if k <= left_len:
-        return __order_stats(left_first, left_second, k)
+        return __order_stats(
+            first=first,
+            first_start=first_start,
+            first_end=left_first_end,
+            second=second,
+            second_start=second_start,
+            second_end=left_second_end,
+            k=k
+        )
     elif left_len < k <= left_len + pivot_len:
         return pivot
     else:
-        return __order_stats(right_first, right_second, k - left_len - pivot_len)
+        return __order_stats(
+            first=first,
+            first_start=right_first_start,
+            first_end=first_end,
+            second=second,
+            second_start=right_second_start,
+            second_end=second_end,
+            k=k - left_len - pivot_len
+        )
 
 
-def median(first: list, second: list) -> float:
+def median(first, second):
     if first is None:
         first = []
 
@@ -40,14 +68,39 @@ def median(first: list, second: list) -> float:
 
     # k-th order statistics index starts from 1
     if overall_len % 2 == 1:
-        return __order_stats(first, second, (overall_len // 2) + 1)
+        return __order_stats(
+            first=first,
+            first_start=0,
+            first_end=len(first),
+            second=second,
+            second_start=0,
+            second_end=len(second),
+            k=(overall_len // 2) + 1
+        )
     else:
         median_first_k = (overall_len // 2)
         median_second_k = (overall_len // 2) + 1
-        return (
-                       __order_stats(first, second, median_first_k) +
-                       __order_stats(first, second, median_second_k)
-               ) / 2
+
+        median_first_value = __order_stats(
+            first=first,
+            first_start=0,
+            first_end=len(first),
+            second=second,
+            second_start=0,
+            second_end=len(second),
+            k=median_first_k
+        )
+        median_second_value = __order_stats(
+            first=first,
+            first_start=0,
+            first_end=len(first),
+            second=second,
+            second_start=0,
+            second_end=len(second),
+            k=median_second_k
+        )
+
+        return (median_first_value + median_second_value) / 2
 
 
 if __name__ == '__main__':
