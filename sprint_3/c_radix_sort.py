c107501d6ca76fd441a3b95bc7e1fc043be61a65
Ilya Sarantsev <ilya.sarantsev@team.wrike.com>
2020-07-20 11:52:59 +0300

    Radix sort solution

---
+++ sprint_3/c_radix_sort.py
@@ -0,0 +1,42 @@
+from typing import List
+
+__max_rank = 6
+
+
+def __counting_sort(arr: List[int], digit_place: int) -> List[int]:
+    sorted_by_rank = [list() for _ in range(10)]
+
+    for n in arr:
+        sorted_by_rank[(n // pow(10, digit_place)) % 10].append(n)
+
+    result = list()
+
+    for rank in sorted_by_rank:
+        result += rank
+
+    return result
+
+
+def radix_sort(arr: List[int]) -> List[int]:
+    digit_place = 0
+    result = arr
+
+    while digit_place <= __max_rank:
+        result = __counting_sort(result, digit_place=digit_place)
+        digit_place += 1
+
+    return result
+
+
+if __name__ == '__main__':
+    with open('input.txt', mode='r') as input_txt, open('output.txt', mode='w') as output_txt:
+        input_txt.readline()
+        sorted = radix_sort(
+            [int(n) for n in input_txt.readline().split(' ')]
+        )
+
+        output_txt.write(
+            ' '.join(
+                [str(n) for n in sorted]
+            )
+        )
---
+++ tests/sprint_3/test_c_radix_sort.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from random import randrange
+
+from sprint_3.c_radix_sort import radix_sort
+
+
+class RadixSortTest(TestCase):
+    def test_empty(self):
+        self.assertEqual([], radix_sort([]))
+
+    def test_single(self):
+        self.assertEqual([1], radix_sort([1]))
+
+    def test_sorted(self):
+        self.assertEqual([3, 8], radix_sort([3, 8]))
+
+    def test_unsorted(self):
+        self.assertEqual([3, 8], radix_sort([8, 3]))
+
+    def test_ranks(self):
+        self.assertEqual([12, 21], radix_sort([21, 12]))
+
+    def test_random(self):
+        for _ in range(randrange(1000)):
+            max_item = 100000
+            max_items = 10000
+
+            unsorted = [
+                randrange(max_item) for _ in range(randrange(max_items))
+            ]
+
+            self.assertEqual(
+                sorted(unsorted),
+                radix_sort(unsorted)
+            )
